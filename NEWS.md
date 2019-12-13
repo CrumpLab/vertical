ce3b5a37cc6abc6a8dc1c5627a30466cb5a0cf95
Matt Crump <crumpmj@gmail.com>
2019-12-13 08:17:29 -0500

    update version
    see news: added yml udpating to build_vertical()

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,10 @@
+# vertical 0.0.0.9200
+
+* added init functions to add individual vertical components to a vertical project from the console
+* added `update_yml()` to scrape vertical component folders and update `_pkgdown.yml` to include .Rmds in each folder and subfolder in respective component tabs on the website
+* extended functionality of `build_vertical()` to update `_pkgdown.yml` on build with `update_yml()`. Also, `build_vertical()` now renders all .Rmds in any main folder of a vertical component. Currently, sub folders are also rendered in vignettes, but not other component folders.
+* Updated getting started tutorial and readme
+
 # vertical 0.0.0.9100
 
 This version is a rethinking of vertical's back-end functionality with the goal of making vertical more lightweight and portable.
--- R/vertical_project.R
+++ R/vertical_project.R
@@ -118,28 +118,119 @@ init_jspsych <- function() {
   file.copy(vertical_jspsych, "experiments/experiment-1/index.html")
 }
 
+#' Update _pkgdown.yml for Rmds in Folders
+#'
+#' @param folder character, name of a vertical folder, e.g., "vignettes"
+#' @param yml_component character, name of the corresponding yml component, e.g., "articles"
+#' @param tab_title character, the title of the component tab written to the website, e.g., "Supplementary"
+#'
+#' @return file, an updated _pkgdown.yml file
+#'
+#' @description This function scrapes a folder and all sub-folders for R markdown documents, and then updates the a designated yml component in _pkgdown.yml with a new list of items. The list of items in shown in the component's tab on the website, and the title of the tab is set to `tab_title`. In the tab, the titles of each .rmd in the main folder are added; then, for each sub-folder the sub-folder name is added (as a text seperator in the tab), followed by the titles for each .rmd in the sub-folder. This function is run automatically by `build_vertical()`.
+#'
+#' @export
+#'
+
+update_yml <- function(folder,yml_component,tab_title){
+
+  if (dir.exists(folder)){
+    # get rmds in folder
+    top_rmds <- list.files(folder, pattern = "\\.Rmd$", full.names = TRUE)
+    top_rmds_yml <- lapply(top_rmds,rmarkdown::yaml_front_matter)
+    top_rmds_title <- unlist(sapply(top_rmds_yml,"[","title"))
+    top_rmds_html <- list.files(folder, pattern = "\\.Rmd$")
+    top_rmds_html <- gsub(".Rmd",".html",top_rmds_html)
+
+    # create temporary articles list
+    new_articles <- list()
+    new_articles <- list(text = tab_title,
+                         menu  = list())
+
+    # add main folder vignettes to list
+    if(length(top_rmds) > 0) {
+      for(i in 1:length(top_rmds_title)){
+        new_articles$menu[[i]] <- list(text = top_rmds_title[i],
+                                       href = paste(folder,top_rmds_html[i],sep="/"))
+      }
+    }
+
+    # add subfolder vignettes
+    sub_folders <- list.dirs(folder, full.names=FALSE, recursive=FALSE)
+    for(i in sub_folders){
+      # get rmds in sub folder
+      sub_rmds <- list.files(paste(folder,i,sep="/"),
+                             pattern = "\\.Rmd$", full.names = TRUE)
+      if(length(sub_rmds) > 0){
+        # add folder name as text separator
+        new_articles$menu[[length(new_articles$menu)+1]] <- list(text = i)
+
+        sub_rmds_yml <- lapply(sub_rmds,rmarkdown::yaml_front_matter)
+        sub_rmds_title <- unlist(sapply(sub_rmds_yml,"[","title"))
+        sub_rmds_html <- list.files(paste(folder,i,sep="/"), pattern = "\\.Rmd$")
+        sub_rmds_html <- gsub(".Rmd",".html",sub_rmds_html)
+
+        # add sub folder vignettes to list
+        if(length(sub_rmds) > 0) {
+          for(j in 1:length(sub_rmds_title)){
+            new_articles$menu[[length(new_articles$menu)+1]] <- list(text = sub_rmds_title[j],
+                                                                     href = paste(folder,i,sub_rmds_html[j],sep="/"))
+          }
+        }
+      }
+    } #end loop
+
+    if( file.exists("_pkgdown.yml") ){
+      temp_yml <- yaml::read_yaml("_pkgdown.yml")
+      temp_yml$navbar$components[[yml_component]] <- new_articles
+      yaml::write_yaml(temp_yml,"_pkgdown.yml")
+    }
+  }
+}
+
 #' Build vertical project
 #'
 #' Build the website associated with a vertical project
 #'
+#' @param clean logical, when clean=TRUE (the default), the `docs` folder is cleaned (e.g., completely wiped) using `pkgdown::clean_site()`, otherwise when clean=FALSE `clean_site()` will not be run.
+#' @param update_yml logical, update_yml=TRUE is the default, updates yml components in `_pkgdown.yml` to list all .Rmds in folders and subfolders of vertical components (manuscript, posters, slides, vignettes) in associated navigation bar tabs on the website.
+#' @param ... params, pass additional parameters to `pkgdown::build_site(...)`
+#'
 #' @export
-build_vertical <- function() {
-  pkgdown::clean_site()
-  pkgdown::build_site()
-  rmarkdown::render(
-    "posters/poster.Rmd",
-    output_dir = "docs/posters", output_file = "poster.html"
-  )
-  rmarkdown::render(
-    "slides/slides.Rmd",
-    output_dir = "docs/slides", output_file = "slides.html"
-  )
-  # Works, but throws error if file not created first
-  file.create("manuscript/r-references.bib")
-  rmarkdown::render(
-    "manuscript/manuscript.Rmd",
-    output_dir = "docs/manuscript"
-  )
+build_vertical <- function(clean=TRUE,update_yml=TRUE,...) {
+  if(update_yml == TRUE) {
+    update_yml("vignettes","articles","Supplementary")
+    update_yml("manuscript","manuscript","PDF")
+    update_yml("posters","posters","Poster")
+    update_yml("slides","slides","Slides")
+  }
+  if(clean == TRUE) pkgdown::clean_site()
+  pkgdown::build_site(...)
+
+  if (dir.exists("posters")) {
+    file_names <- list.files("posters", pattern = "\\.Rmd$")
+    for(i in file_names){
+      rmarkdown::render(paste("posters",i,sep="/"),
+                        output_dir = "docs/posters/")
+    }
+  }
+
+  if (dir.exists("slides")) {
+    file_names <- list.files("slides", pattern = "\\.Rmd$")
+    for(i in file_names){
+      rmarkdown::render(paste("slides",i,sep="/"),
+                        output_dir = "docs/slides/")
+    }
+  }
+
+  if (dir.exists("manuscript")) {
+    # Works, but throws error if file not created first
+    file.create("manuscript/r-references.bib")
+    file_names <- list.files("manuscript", pattern = "\\.Rmd$")
+    for(i in file_names){
+      rmarkdown::render(paste("manuscript",i,sep="/"),
+                        output_dir = "docs/manuscript/")
+    }
+  }
   if (dir.exists("experiments")) {
     dir.create("docs/experiments")
     file.copy("experiments", "docs", recursive = TRUE)
