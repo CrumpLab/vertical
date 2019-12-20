7bf46101b53f6e4e97eaf770f30f54de40fed2dd
Matti Vuorre <mv2521@columbia.edu>
2019-12-20 17:47:13 -0500

    Provide data.R template and processing script that actually works

---
+++ inst/templates/data.R
@@ -0,0 +1,9 @@
+#' @title A dataset
+#' @description Example data frame
+#' @format A data.frame with . rows and . variables:
+#' \describe{
+#'   \item{\code{a}}{Variable a; integer.}
+#'   \item{\code{b}}{Variable b; factor with 2 levels (x, y).}
+#'}
+#' @details This dataset is an example.
+"{{{dataname}}}"
---
+++ inst/templates/preprocess.R
@@ -0,0 +1,8 @@
+## Example data preprocessing script
+
+# Load data
+mydata <- data.frame(a = 1, b = 2)
+
+# Processing ...
+
+usethis::use_data(mydata)
